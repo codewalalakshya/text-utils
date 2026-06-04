d0dc5be3c7fbd2b2eca736223755a21a98bdefc0
codewalalakshya <lakshyapratapsingh2001@gmail.com>
2026-06-04 18:17:51 +0530

    full working project

--- src/App.js
+++ src/App.js
@@ -1,55 +1,96 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import AboutUs from './components/AboutUs';
+import Alert from './components/Alert';
+
 import React, { useState } from 'react';
-import Alert from './components/Alert'
-// import AboutUs from './components/AboutUs';
 
-
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
-   const [mode, setMode] = useState('light');
-   const [alert, setalert] = useState(null);
-   const showAlert = (message ,type )=>{
-     setalert({
-       msg : message,
-       type : type
-     })
-     setTimeout(() => {
-       setalert(null);
-     }, 1500);
-   }
-    const toggleMode = () => {
-      if (mode === 'light') {
-        setMode('dark');
-        document.body.style.backgroundColor = '#2b3740';
-        showAlert("Dark mode has been enabled","success");
-       
-      } else {
-        setMode('light');
-        document.body.style.backgroundColor = 'white';
-        showAlert("Light mode has been enabled","success");
-      }
+
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+
+    if (mode === 'light') {
+
+      setMode('dark');
+      document.body.style.backgroundColor = '#20282e';
+
+      showAlert(
+        "Dark mode has been enabled",
+        "success"
+      );
+
+    } else {
+
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+
+      showAlert(
+        "Light mode has been enabled",
+        "success"
+      );
     }
+  };
+
   return (
-   
-    <>
-     <Navbar
-       heading="TextUtils"
-       title="TextUtils"
-       aboutText="About Us"
-       logo="/logo.png"
-       logoAlt="Textutils logo"
-       mode={mode}
-       toggleMode={toggleMode}
-     />
-     <Alert alert={alert}/>
+    <Router>
+
+      <Navbar
+        title="TextUtils"
+        aboutText="About Us"
+        logo="/logo.png"
+        logoAlt="TextUtils Logo"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+
+      <Alert alert={alert} />
+
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze below"  mode={mode} showAlert={showAlert}/>
-        {/* <AboutUs /> */}
+
+        <Routes>
+
+          <Route
+            path="/"
+            element={
+              <TextForm
+                heading="Enter the text to analyze below"
+                mode={mode}
+                showAlert={showAlert}
+              />
+            }
+          />
+
+          <Route
+            path="/about"
+            element={<AboutUs mode={mode} />}
+          />
+
+        </Routes>
+
       </div>
-    </>
+
+    </Router>
   );
 }
 
